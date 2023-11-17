9d218a4f2f5383fbb8f3687b7d3fece1348e17b3
Brian Esquivel <19151684@aguascalientes.tecnm.mx>
2023-11-17 01:05:15 -0600

    ya me filtra por restaurantes

--- backend/Backend_AppEtito/urls.py
+++ backend/Backend_AppEtito/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RestaurantListView
+
+
 from .views import AdminViewSet, UserViewSet, Login, LocationsViewSet, PaymentMethodViewSet, CategoriesViewSet, RestaurantViewSet, FoodsViewSet, OrdersViewSet
 
 router = DefaultRouter()
@@ -9,12 +10,11 @@ router.register(r'user', UserViewSet)
 router.register(r'locations', LocationsViewSet)
 router.register(r'payment', PaymentMethodViewSet)
 router.register(r'categories', CategoriesViewSet)
-router.register(r'restaurants', RestaurantViewSet, basename='categories_pk')  
+router.register(r'restaurants', RestaurantViewSet, basename='categories_pk')
 router.register(r'foods', FoodsViewSet)
 router.register(r'orders', OrdersViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', Login.as_view(), name='login'),
-    path('api/restaurants/categories/<int:categories_pk>/', RestaurantListView.as_view(), name='restaurant-list-by-category'),
 ]
